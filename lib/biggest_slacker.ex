8201f6f820d3442081c0b0cfdf597b07672c114e
Dave Shah <dave.shah@genomoncology.com>
2016-03-09 21:14:44 -0500

    Keep it short

--- lib/biggest_slacker.ex
+++ lib/biggest_slacker.ex
@@ -6,11 +6,11 @@ defmodule BiggestSlacker do
 
   def users_to_message_counts(list_of_history) do
     list_of_history
-    |> Enum.reduce(%{},fn(history_item, accumulator) -> add_match(accumulator, history_item) end)
+    |> Enum.reduce(%{}, &(add_match(&1, &2)))
     |> Enum.sort(fn({_, v1}, {_, v2}) -> v1 > v2 end)
   end
 
-  defp add_match(map, %{user: user}) do
+  defp add_match(%{user: user}, map) do
     Map.update(map, user, 1, &(&1 + 1))
   end
 
